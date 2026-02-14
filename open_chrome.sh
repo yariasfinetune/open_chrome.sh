231afb22ccc45c7fc29ebfa8cd990996e99ce1bf
Yohan Arias <yohanarias@MacBook-Pro-de-Yohan.local>
2026-02-14 12:55:48 -0500

    First Commit

--- open_chrome.sh
+++ open_chrome.sh
@@ -5,8 +5,8 @@ if [ -z "$environment" ]; then
   exit 1
 fi
 
-if [ "$environment" != "uat" ] && [ "$environment" != "testing" ] && [ "$environment" != "prod" ]; then
-  echo "Environment must be either uat, testing, or prod"
+if [ "$environment" != "local" ] && [ "$environment" != "uat" ] && [ "$environment" != "testing" ] && [ "$environment" != "prod" ]; then
+  echo "Environment must be either local, uat, testing, or prod"
   exit 1
 fi
 
@@ -18,7 +18,7 @@ fi
 
 open -na "Google Chrome" --args \
   --proxy-server="127.0.0.1:8089" \
-  --proxy-bypass-list="*.learnosity.com;reports-va.learnosity.com;sdk.split.io;cdn.split.io;wistia.com;fast.wistia.net;status.us10.clickhelp.co;cb.academicmerit.com;apc-api-uat.collegeboard.org;apc-api-testing.collegeboard.org;apc-api-production.collegeboard.org" \
+  --proxy-bypass-list="54.145.35.252;*.learnosity.com;reports-va.learnosity.com;sdk.split.io;cdn.split.io;wistia.com;fast.wistia.net;status.us10.clickhelp.co;cb.academicmerit.com;apc-api-uat.collegeboard.org;apc-api-testing.collegeboard.org;apc-api-production.collegeboard.org" \
   --user-data-dir="/tmp/chrome-mitm" \
   --ignore-certificate-errors \
   "https://${target_host}/login?directlogin=true"
